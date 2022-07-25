08476bff8801aeb5364b9b841ed0e5ae3dae3a6f
Bada-S <jamessopkin@gmail.com>
2022-07-25 09:50:43 -0500

    add db

--- main.tf
+++ main.tf
@@ -1,49 +1,16 @@
 terraform {
-  required_version = ">= 0.15.0"
-
-  backend "remote" {
-    organization = "jamessopkin"
-
-    workspaces {
-      name = "snowflake"
-    }
-  }
-
   required_providers {
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.32.0"
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.35"
     }
   }
 }
 
-data "terraform_remote_state" "core" {
-  backend = "remote"
-
-  config = {
-    organization = "jamessopkin"
-
-    workspaces = {
-      name = "snowflake"
-    }
-  }
+provider "snowflake" {
+  role = "SYSADMIN"
 }
 
-
-provider "snowflake" {
-  account  = "mt35412"
-  region   = "us-east-2"
-  role     = "SECURITYADMIN"
-  alias    = "security"
-  username = var.SNOWFLAKE_USER
-  password = var.SNOWFLAKE_PASSWORD
-}
-
-provider "snowflake" {
-  account  = "mt35412"
-  region   = "us-east-2"
-  role     = "SYSADMIN"
-  alias    = "system"
-  username = var.SNOWFLAKE_USER
-  password = var.SNOWFLAKE_PASSWORD
+resource "snowflake_database" "db" {
+  name = "FIVETRAN_DB"
 }
